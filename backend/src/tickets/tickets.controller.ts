2a4782bcdba255f3f5b648a96b8313b2a3b88f69
timmdkrtf <m.hamdanhariri5@gmail.com>
2026-06-23 14:49:43 +0700

    feat: implement create ticket endpoint

--- backend/src/tickets/tickets.controller.ts
+++ backend/src/tickets/tickets.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, UseGuards, Post, Get, } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  UseGuards,
+  Post,
+  Get,
+  Param,
+  Request,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { CreateTicketDto } from './dto/create-ticket.dto';
 import { TicketsService } from './tickets.service';
 
-@UseGuards(JwtAuthGuard) // Add appropriate guards here, e.g., JwtAuthGuard
+@UseGuards(JwtAuthGuard)
 @Controller('tickets')
 export class TicketsController {
-  constructor(
-    private readonly ticketsService: TicketsService,
-  ) {}
+  constructor(private readonly ticketsService: TicketsService) {}
 
   @Post()
-  create() {}
+  create(@Body() createTicketDto: CreateTicketDto, @Request() req) {
+    return this.ticketsService.create(createTicketDto, req.user.id);
+  }
 
   @Get()
-  findAll() {}
+  findAll() {
+    return this.ticketsService.findAll();
+  }
 
   @Get(':id')
-  findOne() {}
+  findOne(@Param('id') id: string) {
+    return this.ticketsService.findOne(id);
+  }
 }
--- backend/src/tickets/tickets.service.ts
+++ backend/src/tickets/tickets.service.ts
@@ -1,4 +1,62 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+
 @Injectable()
-export class TicketsService {}
+export class TicketsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createTicketDto: CreateTicketDto, userId: string) {
+    return this.prisma.ticket.create({
+      data: {
+        ...createTicketDto,
+        createdById: userId,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.ticket.findMany({
+      include: {
+        createdBy: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+        assignedTo: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+  }
+
+  async findOne(id: string) {
+    return this.prisma.ticket.findUnique({
+      where: { id },
+      include: {
+        createdBy: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+        assignedTo: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+        comments: true,
+      },
+    });
+  }
+}
