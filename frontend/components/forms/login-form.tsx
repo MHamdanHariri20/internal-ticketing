8fa073d26007d752b7785c19322f13d11f501da8
timmdkrtf <m.hamdanhariri5@gmail.com>
2026-06-24 09:20:33 +0700

    feat: set up axios

---
+++ frontend/components/forms/login-form.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { LoginFormValues, loginSchema } from "@/lib/validations/auth";
+import { FormError } from "@/components/common/form-error";
+import { login } from "@/services/auth.service";
+
+export default function LoginForm() {
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: LoginFormValues) => {
+    try {
+      const response = await login(values);
+
+      console.log(response);
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  return (
+    <Card className="shadow-sm">
+      <CardHeader className="space-y-2">
+        <CardTitle className="text-xl font-bold">Welcome Back</CardTitle>
+        <CardDescription className="text-sm">
+          Sign in to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <form className="space-y-5" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              {...form.register("email")}
+              type="email"
+              placeholder="john@example.com"
+            />
+            <FormError message={form.formState.errors.email?.message} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              {...form.register("password")}
+              type="password"
+              placeholder="••••••••"
+            />
+            <FormError message={form.formState.errors.password?.message} />
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? "Signing In..." : "Sign In"}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -1,10 +1,10 @@
-import api from  "@/utils/api";
+import api from  "@/lib/axios";
 import { LoginDto, LoginResponse } from "@/types/auth";
 
 export const login = async (
   data: LoginDto,
 ): Promise<LoginResponse> => {
-  const response = await api.post("/auth/login", data);
+  const response = await api.post<LoginResponse>("/auth/login", data);
 
   return response.data;
 };
